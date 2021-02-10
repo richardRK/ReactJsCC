a7787c54ed6effe9bfdbb024df1f309f7c554bae
rohi <richardRK@users.noreply.github.com>
2021-02-10 17:11:50 +0530

    initialSetup

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+
+const x = true;
+const name = 'Rohi';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <h1>React {x ? "Yes" : "No"}</h1>
     </div>
   );
 }
