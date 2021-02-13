b45bc47924c72d42b2a9790c84fbb313c850936d
rohi <richardRK@users.noreply.github.com>
2021-02-13 19:09:28 +0530

    onToggle

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -36,15 +36,25 @@ const App = () => {
     },
   ]);
 
+  //delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
 
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "List is Empty"
       )}
