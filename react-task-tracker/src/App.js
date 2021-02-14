0886ec1b32a58874690de1fad039c1250852b7c6
rohi <richardRK@users.noreply.github.com>
2021-02-14 21:46:57 +0530

    useLocation

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -121,7 +121,7 @@ const App = () => {
             onAdd={() => setShowAddTask(!showAddTask)}
             showAdd={showAddTask}
           />
-          {showAddTask && <AddTask onAdd={addTask} />}
+          {/* {showAddTask && <AddTask onAdd={addTask} />}
           {tasks.length > 0 ? (
             <Tasks
               tasks={tasks}
@@ -130,7 +130,26 @@ const App = () => {
             />
           ) : (
             "List is Empty"
-          )}
+          )} */}
+
+          <Route
+            path="/"
+            exact
+            render={(props) => (
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  "No Tasks To Show"
+                )}
+              </>
+            )}
+          />
 
           <Route path="/about" component={About} />
           <Footer />
